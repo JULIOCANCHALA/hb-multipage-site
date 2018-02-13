64f73f4b8caa79676dc76506a7570c9b1f61214c
JULIOCANCHALA <juliofernando1c@hotmail.com>
2018-02-13 16:34:13 -0500

    working in Mainnav.js

--- src/modules/main-nav/MainNav.js
+++ src/modules/main-nav/MainNav.js
@@ -7,5 +7,30 @@ export default class Mainnav {
   constructor (node) {
     this.node = node
     this.node.innerHTML = template(mainNav)
+    this.elements = {}
+    this.start()
+    this.setClicks()
+  }
+
+  start () {
+    this.elements.hamIcon = this.node.querySelector('.hamburguer-icon-btn')
+    this.elements.menu = document.querySelector('.menu')
+    this.elements.btnsMenu = document.querySelectorAll('.menu__item--btn')
+    this.elements.subItems = document.querySelectorAll('.menu__item--sub')
+  }
+  setClicks () {
+    this.elements.hamIcon.addEventListener('click', () => {
+      this.elements.menu.classList.toggle('menu--visible')
+    })
+
+    this.elements.btnsMenu.forEach(element => {
+      element.addEventListener('click', this.opensubMenu.bind(this))
+    })
+  }
+  opensubMenu (event) {
+    console.log(event.target.textContent)
+    this.elements.subItems.forEach(element => {
+      console.log(element.dataset)
+    })
   }
 }
